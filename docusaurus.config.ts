eb3939d58710b2f4cbe199b7b4becd70a867b774
Ahmed AbouZaid <6760103+aabouzaid@users.noreply.github.com>
2025-11-25 06:01:21 +0100

    changed: upgrade docusaurus version from 3.7.0 to 3.9.2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,11 +15,16 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'DevOpsHiveHQ', // Usually your GitHub org/user name.
-  projectName: 'dynamic-devops-roadmap', // Usually your repo name.
+  organizationName: 'DevOpsHiveHQ',
+  projectName: 'dynamic-devops-roadmap',
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
