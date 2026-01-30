047d88047c3f37e31744ae3accb406902f2d1704
mohamed khaled <mohamedkhaledworkmail@gmail.com>
2026-01-30 22:04:15 +0200

    refactor: clean repo and add basic app v0.0.1 with Docker

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+
+def get_version():
+    """Returns the current application version."""
+    return "v0.0.1"
+
+def main():
+    """Main entry point of the application."""
+    print(get_version())
+
+if __name__ == "__main__":
+    main()
+    
--- src/components/Audience/index.tsx
+++
@@ -1,3 +0,0 @@
-export {default as AudienceSwitch} from './AudienceSwitch';
-export {default as DevOpsRoleTopics} from './DevOpsRoleTopics';
-export {default as TargetAudience} from './TargetAudience';
